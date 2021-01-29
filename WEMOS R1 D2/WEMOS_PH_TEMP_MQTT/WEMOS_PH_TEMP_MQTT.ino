e70af4576ec22e4386302e0889356725175af267
Fábio Alves <fbio.alves095@gmail.com>
2021-01-28 21:26:17 -0300

    Wemos R1 D2 examples

---
+++ WEMOS R1 D2/WEMOS_PH_TEMP_MQTT/WEMOS_PH_TEMP_MQTT.ino
@@ -0,0 +1,154 @@
+#include <OneWire.h>                
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// NETWORK VARIABLES
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+unsigned long lastMsg = 0;
+const char* ssid = "BBG_JOSELITO";
+const char* password = "Singularidade10";
+const char* mqtt_server = "tccmqtt.duckdns.org";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// SENSOR VARIABLES
+OneWire ourWire(D8);                
+DallasTemperature tempSensor(&ourWire);
+float calibration = 23.00;
+const int analogInPin = A0;
+int sensorValue = 0;
+unsigned long int avgValue;
+float b;
+int buf[10], temp;
+
+float read_ph() {
+  for (int i = 0; i < 10; i++) {
+    buf[i] = analogRead(analogInPin);
+    delay(30);
+  }
+  
+  for (int i = 0; i < 9; i++) {
+    for (int j = i + 1; j < 10; j++) {
+      if (buf[i] > buf[j]) {
+        temp = buf[i];
+        buf[i] = buf[j];
+        buf[j] = temp;
+      }
+    }
+  }
+  
+  avgValue = 0;
+  for (int i = 2; i < 8; i++)
+  avgValue += buf[i];
+  float pHVol = (float)avgValue * 3.3 / 1024 / 6;
+  float phValue = -5.70 * pHVol + calibration;
+  Serial.print("PH = ");
+  Serial.println(phValue);
+  return phValue;
+}
+
+float read_temp() {
+  tempSensor.requestTemperatures();  
+  float tempValue= tempSensor.getTempCByIndex(0);
+  Serial.print("Temp = ");
+  Serial.print(tempValue);
+  Serial.println(" C");
+  return tempValue;
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  tempSensor.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  delay(1000);
+  
+}
+ 
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    float phValue = read_ph();
+    float tempValue = read_temp();
+    snprintf(msg, MSG_BUFFER_SIZE, "{\"temperature\":%f,\"ph\":%f}", tempValue, phValue);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }                 
+}
