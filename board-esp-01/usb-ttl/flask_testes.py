add86a9ed863f4b8a032d75bcf41358377f0e90c
--replace-all <fbio.alves095@gmail.com>
2021-07-12 14:26:24 -0300

    Rota HTTP POST no flask

--- board-esp-01/usb-ttl/flask_testes.py
+++ board-esp-01/usb-ttl/flask_testes.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def function():
         return "hello, world!"
+
+@app.route('/post', methods=['POST'])
+def function2():
+        return jsonify({
+		'int':0,
+		'str':'foo'
+	})
+
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=80, debug=True)
