3cd623a55dddfbac40d9694c388a40bf098699aa
Fábio Alves <fbio.alves095@gmail.com>
2022-10-31 11:56:41 -0300

    ponte h

--- ponte_h/ponte_h.ino
+++ ponte_h/ponte_h.ino
@@ -1,11 +1,11 @@
 // Definindo pinos do NodeMcu para driver Ponte H
 // Pinos de controle motor A
-int IN1 = 14;   // pwm signal motor 1
-int IN2 = 27;   // direction motor 1 = horario, 0 = anti-horario
+int IN1 = 13;   // pwm signal motor 1
+int IN2 = 12;   // direction motor 1 = horario, 0 = anti-horario
 
 // Pinos de controle motor B
-int IN3 = 13;   // pwm signal motor 2
-int IN4 = 12;   // direction motor 2 = horario, 0 = anti-horario
+int IN3 = 14;   // pwm signal motor 2
+int IN4 = 27;   // direction motor 2 = horario, 0 = anti-horario
  
 void setup()
 {
@@ -18,7 +18,7 @@ void setup()
  
 void loop()
 {
-  /*
+
  //Gira o Motor A no sentido horario
  digitalWrite(IN1, HIGH);
  digitalWrite(IN2, LOW);
@@ -38,9 +38,9 @@ void loop()
  digitalWrite(IN1, HIGH);
  digitalWrite(IN2, HIGH);
  delay(500); //aguarda 500 ms
-*/
+
  
- 
+ /*
  //Gira o Motor B no sentido horario
  digitalWrite(IN3, HIGH);
  digitalWrite(IN4, LOW);
@@ -60,5 +60,5 @@ void loop()
  digitalWrite(IN3, HIGH);
  digitalWrite(IN4, HIGH);
  delay(500); //aguarda 500ms
- 
+ */
 }
